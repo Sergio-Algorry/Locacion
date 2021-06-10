2ce44159563b3999d0aa59a07fb087c1d30f7bf9
Sergio Algorry <40408779+Sergio-Algorry@users.noreply.github.com>
2021-06-10 17:37:15 -0300

    Migraciones

---
+++ Locacion/Shared/Migrations/20210607141313_CompletaTablaPais.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Locacion.Comunes.Migrations
+{
+    public partial class CompletaTablaPais : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "NombrePais",
+                table: "Paises",
+                type: "nvarchar(120)",
+                maxLength: 120,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CodPais",
+                table: "Paises",
+                type: "nvarchar(2)",
+                maxLength: 2,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "UQ_Pais_CodPais",
+                table: "Paises",
+                column: "CodPais",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "UQ_Pais_CodPais",
+                table: "Paises");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NombrePais",
+                table: "Paises",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(120)",
+                oldMaxLength: 120);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CodPais",
+                table: "Paises",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(2)",
+                oldMaxLength: 2);
+        }
+    }
+}
---
+++ Locacion/Shared/Migrations/20210607143737_Provincia.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Locacion.Comunes.Migrations
+{
+    public partial class Provincia : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Provincias",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CodProvincia = table.Column<string>(type: "nvarchar(1)", maxLength: 1, nullable: false),
+                    NombreProvincia = table.Column<string>(type: "nvarchar(120)", maxLength: 120, nullable: false),
+                    PaisId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Provincias", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Provincias_Paises_PaisId",
+                        column: x => x.PaisId,
+                        principalTable: "Paises",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Provincias_PaisId",
+                table: "Provincias",
+                column: "PaisId");
+
+            migrationBuilder.CreateIndex(
+                name: "UQ_Provincia_CodProvincia",
+                table: "Provincias",
+                column: "CodProvincia",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Provincias");
+        }
+    }
+}
